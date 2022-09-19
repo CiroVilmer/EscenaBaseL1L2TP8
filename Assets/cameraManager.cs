cde6a51cd8e6be6b306ba4a4f6ee1e8dde8625f6
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-09-19 12:04:15 -0300

    class 2

---
+++ Assets/cameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraManager : MonoBehaviour
+{
+    public GameObject camIntro;
+    public GameObject camPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camIntro.SetActive(true);
+        camPlayer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CameraActivaion()
+    {
+        camIntro.SetActive(false);
+        camPlayer.SetActive(true);
+    }
+}
---
+++ Assets/carroNetsScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carroNetsScript : MonoBehaviour
+{
+    public GameObject computerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstantiateComputer()
+    {
+        Instantiate(computerPrefab, new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.rotation);
+    }
+}
